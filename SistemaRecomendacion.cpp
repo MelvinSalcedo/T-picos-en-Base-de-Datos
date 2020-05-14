2bef7d108c2ed73fbb559276162ea6b5a683ad47
Melvin salcedo <melvinsalcedo169@gmail.com>
2020-05-14 16:24:12 -0500

    correcion de distancias

--- SistemaRecomendacion.cpp
+++ SistemaRecomendacion.cpp
@@ -12,7 +12,11 @@ Int DistanciaManhatan(Int SujetoA[], Int SujetoB[],int contador){
     Int Distancia=0;
 
     for (int i=0;i<contador;i++){
-        Distancia+=abs(SujetoA[i]-SujetoB[i]);
+        if(SujetoA[i]!=-1 && SujetoB[i]!=-1){
+                //cout<<SujetoA[i]<<" - "<<SujetoB[i]<<endl;
+            Distancia+=abs(SujetoA[i]-SujetoB[i]);
+        }
+
     }
     return Distancia;
 }
@@ -21,7 +25,10 @@ Int DistanciaEuclidiana(Int SujetoA[], Int SujetoB[],int contador){
     Int Distancia =0;
 
     for (int i=0;i<contador;i++){
-        Distancia+=pow((SujetoA[i]-SujetoB[i]),2);
+        if(SujetoA[i]!=-1 && SujetoB[i]!=-1){
+                //cout<<SujetoA[i]<<" - "<<SujetoB[i]<<" = "<<pow((SujetoA[i]-SujetoB[i]),2)<<endl;
+            Distancia+=pow((SujetoA[i]-SujetoB[i]),2);
+        }
     }
     Distancia=sqrt(Distancia);
     return Distancia;
@@ -29,12 +36,13 @@ Int DistanciaEuclidiana(Int SujetoA[], Int SujetoB[],int contador){
 
 Int DistancaMinkowski(Int SujetoA[], Int SujetoB[],int contador){
     Int sumatoria=0;
-
-    Int r = 1;
-
+    Int r = 2;
     for (int i=0;i<contador;i++){
 
-        sumatoria+=pow(abs(SujetoA[i]-SujetoB[i]),r);
+        if(SujetoA[i]!=-1 && SujetoB[i]!=-1){
+            cout<<SujetoA[i]<<" - "<<SujetoB[i]<<" = "<<pow(abs(SujetoA[i]-SujetoB[i]),r)<<endl;
+            sumatoria+=pow(abs(SujetoA[i]-SujetoB[i]),r);
+        }
     }
     Int Result= pow(sumatoria,1/r);
 
@@ -43,13 +51,13 @@ Int DistancaMinkowski(Int SujetoA[], Int SujetoB[],int contador){
 
 int main (){
 
-    Int Hailey  [] = {0,4,1,4,0,0,4,1};
-    Int Veronica[] = {3,0,0,5,4,2.5,3,0};
+    Int Hailey  [] = {-1,4,1,4,-1,-1,4,1};
+    Int Veronica[] = {3,-1,-1,5,4,2.5,3,-1};
 
-    Int Jordyn  [] = {0,4.5,4,5,5,4.5,5,5};
+    Int Jordyn  [] = {-1,4.5,4,5,5,4.5,4,4};
 
-    Int Angelica[] = {3.5,2  ,0 ,4.5, 5, 1.5, 2.5, 2};
-    Int Bill    [] = {2  ,3.5,4,0,2 ,3.5,0,3 };
+    Int Angelica[] = {3.5,2  ,-1 ,4.5, 5, 1.5, 2.5, 2};
+    Int Bill    [] = {2  ,3.5,4,-1,2 ,3.5,-1,3 };
 
 
     int Len=sizeof(Hailey)/sizeof(int);
